595e398cb0dc7fa2a87a46e6dca8af27fad2fd8d
matthcol <matthias.colin@gmail.com>
2025-07-07 16:51:22 +0200

    compose db + api

---
+++ composition/api/src/main.py
@@ -0,0 +1,21 @@
+from typing import Union
+
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Everyone, summer 2025"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+# mirroring env variable
+@app.get("/db")
+def read_db_url():
+    return {"url": os.environ['DB_URL']}
